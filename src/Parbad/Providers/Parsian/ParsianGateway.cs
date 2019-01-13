0e6641c5a52ae01dcce1122fc94bd75602162074
Sina Soltani <soltani.sina1989@live.com>
2019-01-13 15:03:24 +0100

    Reduce Code Duplication. Code clean up.

--- src/Parbad/Providers/Parsian/ParsianGateway.cs
+++ src/Parbad/Providers/Parsian/ParsianGateway.cs
@@ -2,17 +2,12 @@
 using System.Threading.Tasks;
 using Parbad.Configurations;
 using Parbad.Core;
-using Parbad.Infrastructure.Translating;
-using Parbad.Providers.Parsian.ResultTranslators;
 using Parbad.Utilities;
 
 namespace Parbad.Providers.Parsian
 {
     internal class ParsianGateway : GatewayBase
     {
-        private const string PaymentPageUrl = "https://pec.shaparak.ir/pecpaymentgateway/";
-        private const string WebServiceUrl = "https://pec.shaparak.ir/pecpaymentgateway/eshopservice.asmx";
-
         protected ParsianGatewayConfiguration ParsianConfiguration => ParbadConfiguration.Gateways.GetParsianGatewayConfiguration();
 
         public ParsianGateway() : base(Gateway.Parsian.ToString())
@@ -21,264 +16,86 @@ namespace Parbad.Providers.Parsian
 
         public override RequestResult Request(Invoice invoice)
         {
-            string webServiceXml = CreateRequestWebServiceXml(
-                pin: ParsianConfiguration.Pin,
-                orderId: invoice.OrderNumber,
-                amount: invoice.Amount,
-                callBackUrl: invoice.CallbackUrl);
+            if (invoice == null) throw new ArgumentNullException(nameof(invoice));
 
-            var result = WebHelper.SendXmlWebRequest(WebServiceUrl, webServiceXml);
+            var data = ParsianHelper.CreateRequestData(ParsianConfiguration, invoice);
 
-            long authority = Convert.ToInt64(XmlHelper.GetNodeValueFromXml(result, "authority", "http://tempuri.org/"));
+            var response = WebHelper.SendXmlWebRequest(ParsianHelper.WebServiceUrl, data);
 
-            byte status = Convert.ToByte(XmlHelper.GetNodeValueFromXml(result, "status", "http://tempuri.org/"));
-
-            var isSucceed = status == 0;
-
-            IGatewayResultTranslator gatewayResultTranslator = new ParsianStatusTranslator();
-
-            var translatedResult = gatewayResultTranslator.Translate(status);
-
-            if (!isSucceed)
-            {
-                return new RequestResult(RequestResultStatus.Failed, translatedResult, authority.ToString());
-            }
-
-            var paymentPageUrl = CreatePaymentPageUrl(PaymentPageUrl, authority);
-
-            return new RequestResult(RequestResultStatus.Success, translatedResult, authority.ToString())
-            {
-                BehaviorMode = GatewayRequestBehaviorMode.Redirect,
-                Content = paymentPageUrl
-            };
+            return ParsianHelper.CreateRequestResult(response);
         }
 
         public override async Task<RequestResult> RequestAsync(Invoice invoice)
         {
-            string webServiceXml = CreateRequestWebServiceXml(
-                pin: ParsianConfiguration.Pin,
-                orderId: invoice.OrderNumber,
-                amount: invoice.Amount,
-                callBackUrl: invoice.CallbackUrl);
+            if (invoice == null) throw new ArgumentNullException(nameof(invoice));
 
-            var result = await WebHelper.SendXmlWebRequestAsync(WebServiceUrl, webServiceXml);
+            var data = ParsianHelper.CreateRequestData(ParsianConfiguration, invoice);
 
-            long authority = Convert.ToInt64(XmlHelper.GetNodeValueFromXml(result, "authority", "http://tempuri.org/"));
+            var response = await WebHelper.SendXmlWebRequestAsync(ParsianHelper.WebServiceUrl, data);
 
-            byte status = Convert.ToByte(XmlHelper.GetNodeValueFromXml(result, "status", "http://tempuri.org/"));
-
-            var isSucceed = status == 0;
-
-            IGatewayResultTranslator gatewayResultTranslator = new ParsianStatusTranslator();
-
-            var translatedResult = gatewayResultTranslator.Translate(status);
-
-            if (!isSucceed)
-            {
-                return new RequestResult(RequestResultStatus.Failed, translatedResult, authority.ToString());
-            }
-
-            var paymentPageUrl = CreatePaymentPageUrl(PaymentPageUrl, authority);
-
-            return new RequestResult(RequestResultStatus.Success, translatedResult, authority.ToString())
-            {
-                BehaviorMode = GatewayRequestBehaviorMode.Redirect,
-                Content = paymentPageUrl
-            };
+            return ParsianHelper.CreateRequestResult(response);
         }
 
         public override VerifyResult Verify(GatewayVerifyPaymentContext verifyPaymentContext)
         {
-            //  Callback
+            if (verifyPaymentContext == null) throw new ArgumentNullException(nameof(verifyPaymentContext));
 
-            var referenceId = verifyPaymentContext.RequestParameters.GetAs<string>("au");
+            var callbackResult = ParsianHelper.CreateCallbackResult(verifyPaymentContext);
 
-            var response = verifyPaymentContext.RequestParameters.GetAs<string>("rs");
-
-            if (referenceId.IsNullOrWhiteSpace() || response.IsNullOrWhiteSpace())
+            if (!callbackResult.IsSucceed)
             {
-                return new VerifyResult(Gateway.Parsian, referenceId, string.Empty, VerifyResultStatus.Failed, "Invalid data is received from the gateway.");
+                return callbackResult.Result;
             }
 
-            IGatewayResultTranslator gatewayResultTranslator = new ParsianStatusTranslator();
+            //  Verify
 
-            var isSuccess = response != "0";
+            var data = ParsianHelper.CreateVerifyData(ParsianConfiguration, callbackResult);
 
-            if (!isSuccess)
-            {
-                var callbackTranslatedMessage = gatewayResultTranslator.Translate(Convert.ToByte(response));
+            var response = WebHelper.SendXmlWebRequest(ParsianHelper.WebServiceUrl, data);
 
-                return new VerifyResult(Gateway.Parsian, referenceId, string.Empty, VerifyResultStatus.Failed, callbackTranslatedMessage);
-            }
-
-            //  Now verify
-
-            string verifyWebServiceXml = CreateVerifyWebServiceXml(
-                pin: ParsianConfiguration.Pin,
-                authority: Convert.ToInt64(referenceId));
-
-            var verifyResult = WebHelper.SendXmlWebRequest(WebServiceUrl, verifyWebServiceXml);
-
-            byte status = Convert.ToByte(XmlHelper.GetNodeValueFromXml(verifyResult, "status", "http://tempuri.org/"));
-
-            //  invoiceNumber is TransactionId
-            var transactionId = XmlHelper.GetNodeValueFromXml(verifyResult, "invoiceNumber", "http://tempuri.org/");
-
-            var translatedMessage = gatewayResultTranslator.Translate(status);
-
-            isSuccess = status == 0;
-
-            var verifyResultStatus = isSuccess ? VerifyResultStatus.Success : VerifyResultStatus.Failed;
-
-            return new VerifyResult(Gateway.Parsian, referenceId, transactionId, verifyResultStatus, translatedMessage);
+            return ParsianHelper.CreateVerifyResult(response, callbackResult);
         }
 
         public override async Task<VerifyResult> VerifyAsync(GatewayVerifyPaymentContext verifyPaymentContext)
         {
-            //  Callback
+            if (verifyPaymentContext == null) throw new ArgumentNullException(nameof(verifyPaymentContext));
 
-            var referenceId = verifyPaymentContext.RequestParameters.GetAs<string>("au");
+            var callbackResult = ParsianHelper.CreateCallbackResult(verifyPaymentContext);
 
-            var response = verifyPaymentContext.RequestParameters.GetAs<string>("rs");
-
-            if (referenceId.IsNullOrWhiteSpace() || response.IsNullOrWhiteSpace())
+            if (!callbackResult.IsSucceed)
             {
-                return new VerifyResult(Gateway.Parsian, referenceId, string.Empty, VerifyResultStatus.Failed, "Invalid data is received from the gateway.");
+                return callbackResult.Result;
             }
 
-            IGatewayResultTranslator gatewayResultTranslator = new ParsianStatusTranslator();
+            //  Verify
 
-            var isSuccess = response != "0";
+            var data = ParsianHelper.CreateVerifyData(ParsianConfiguration, callbackResult);
 
-            if (!isSuccess)
-            {
-                var callbackTranslatedMessage = gatewayResultTranslator.Translate(Convert.ToByte(response));
+            var response = await WebHelper.SendXmlWebRequestAsync(ParsianHelper.WebServiceUrl, data);
 
-                return new VerifyResult(Gateway.Parsian, referenceId, string.Empty, VerifyResultStatus.Failed, callbackTranslatedMessage);
-            }
-
-            //  Now verify
-
-            string verifyWebServiceXml = CreateVerifyWebServiceXml(
-                pin: ParsianConfiguration.Pin,
-                authority: Convert.ToInt64(referenceId));
-
-            var verifyResult = await WebHelper.SendXmlWebRequestAsync(WebServiceUrl, verifyWebServiceXml);
-
-            byte status = Convert.ToByte(XmlHelper.GetNodeValueFromXml(verifyResult, "status", "http://tempuri.org/"));
-
-            //  invoiceNumber is TransactionId
-            var transactionId = XmlHelper.GetNodeValueFromXml(verifyResult, "invoiceNumber", "http://tempuri.org/");
-
-            var translatedMessage = gatewayResultTranslator.Translate(status);
-
-            isSuccess = status == 0;
-
-            var verifyResultStatus = isSuccess ? VerifyResultStatus.Success : VerifyResultStatus.Failed;
-
-            return new VerifyResult(Gateway.Parsian, referenceId, transactionId, verifyResultStatus, translatedMessage);
+            return ParsianHelper.CreateVerifyResult(response, callbackResult);
         }
 
         public override RefundResult Refund(GatewayRefundPaymentContext refundPaymentContext)
         {
-            string webServiceXml = CreateReverseWebServiceXml(ParsianConfiguration.Pin,
-                refundPaymentContext.OrderNumber,
-                refundPaymentContext.Amount);
+            if (refundPaymentContext == null) throw new ArgumentNullException(nameof(refundPaymentContext));
 
-            var result = WebHelper.SendXmlWebRequest(WebServiceUrl, webServiceXml);
+            var data = ParsianHelper.CreateRefundData(ParsianConfiguration, refundPaymentContext);
 
-            byte status = Convert.ToByte(XmlHelper.GetNodeValueFromXml(result, "status", "http://tempuri.org/"));
+            var response = WebHelper.SendXmlWebRequest(ParsianHelper.WebServiceUrl, data);
 
-            IGatewayResultTranslator gatewayResultTranslator = new ParsianStatusTranslator();
-
-            var translatedMessage = gatewayResultTranslator.Translate(status);
-
-            var isSuccess = status == 0;
-
-            var reverseResultStatus = isSuccess ? RefundResultStatus.Success : RefundResultStatus.Failed;
-
-            return new RefundResult(Gateway.Parsian, refundPaymentContext.Amount, reverseResultStatus, translatedMessage);
+            return ParsianHelper.CreateRefundResult(response, refundPaymentContext);
         }
 
         public override async Task<RefundResult> RefundAsync(GatewayRefundPaymentContext refundPaymentContext)
         {
-            string webServiceXml = CreateReverseWebServiceXml(ParsianConfiguration.Pin,
-                refundPaymentContext.OrderNumber,
-                refundPaymentContext.Amount);
+            if (refundPaymentContext == null) throw new ArgumentNullException(nameof(refundPaymentContext));
 
-            var result = await WebHelper.SendXmlWebRequestAsync(WebServiceUrl, webServiceXml);
+            var data = ParsianHelper.CreateRefundData(ParsianConfiguration, refundPaymentContext);
 
-            byte status = Convert.ToByte(XmlHelper.GetNodeValueFromXml(result, "status", "http://tempuri.org/"));
+            var response = await WebHelper.SendXmlWebRequestAsync(ParsianHelper.WebServiceUrl, data);
 
-            IGatewayResultTranslator gatewayResultTranslator = new ParsianStatusTranslator();
-
-            var translatedMessage = gatewayResultTranslator.Translate(status);
-
-            var isSuccess = status == 0;
-
-            var reverseResultStatus = isSuccess ? RefundResultStatus.Success : RefundResultStatus.Failed;
-
-            return new RefundResult(Gateway.Parsian, refundPaymentContext.Amount, reverseResultStatus, translatedMessage);
-        }
-
-        private static string CreateRequestWebServiceXml(string pin, long orderId, long amount, string callBackUrl)
-        {
-            return
-                "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tem=\"http://tempuri.org/\">" +
-                "<soapenv:Header/>" +
-                "<soapenv:Body>" +
-                "<tem:PinPaymentRequest>" +
-                "<!--Optional:-->" +
-                $"<tem:pin>{pin}</tem:pin>" +
-                $"<tem:amount>{amount}</tem:amount>" +
-                $"<tem:orderId>{orderId}</tem:orderId>" +
-                "<!--Optional:-->" +
-                $"<tem:callbackUrl>{callBackUrl}</tem:callbackUrl>" +
-                "<tem:authority>0</tem:authority>" +
-                "<tem:status>0</tem:status>" +
-                "</tem:PinPaymentRequest>" +
-                "</soapenv:Body>" +
-                "</soapenv:Envelope>";
-        }
-
-        private static string CreatePaymentPageUrl(string paymentPageUrl, long authority)
-        {
-            return $"{paymentPageUrl}?au={authority}";
-        }
-
-        private static string CreateVerifyWebServiceXml(string pin, long authority)
-        {
-            return
-                "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:tem=\"http://tempuri.org/\">" +
-                "<soap:Header/>" +
-                "<soap:Body>" +
-                "<tem:PaymentEnquiry>" +
-                "<!--Optional:-->" +
-                $"<tem:pin>{pin}</tem:pin>" +
-                $"<tem:authority>{authority}</tem:authority>" +
-                "<tem:status>0</tem:status>" +
-                "<tem:invoiceNumber>0</tem:invoiceNumber>" +
-                "</tem:PaymentEnquiry>" +
-                "</soap:Body>" +
-                "</soap:Envelope>";
-        }
-
-        private static string CreateReverseWebServiceXml(string pin, long orderId, long amount)
-        {
-            return
-                "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tem=\"http://tempuri.org/\">" +
-                "<soapenv:Header/>" +
-                "<soapenv:Body>" +
-                "<tem:PinRefundPayment>" +
-                "<!--Optional:-->" +
-                $"<tem:pin>{pin}</tem:pin>" +
-                $"<tem:orderId>{orderId}</tem:orderId>" +
-                $"<tem:orderToRefund>{orderId}</tem:orderToRefund>" +
-                $"<tem:amount>{amount}</tem:amount>" +
-                "<tem:status>0</tem:status>" +
-                "</tem:PinRefundPayment>" +
-                "</soapenv:Body>" +
-                "</soapenv:Envelope>";
+            return ParsianHelper.CreateRefundResult(response, refundPaymentContext);
         }
     }
 }
---
+++ src/Parbad/Providers/Parsian/ParsianHelper.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using Parbad.Core;
+using Parbad.Infrastructure.Translating;
+using Parbad.Providers.Parsian.Models;
+using Parbad.Providers.Parsian.ResultTranslators;
+using Parbad.Utilities;
+
+namespace Parbad.Providers.Parsian
+{
+    internal static class ParsianHelper
+    {
+        private const string PaymentPageUrl = "https://pec.shaparak.ir/pecpaymentgateway/";
+        public const string WebServiceUrl = "https://pec.shaparak.ir/pecpaymentgateway/eshopservice.asmx";
+
+        public static string CreateRequestData(ParsianGatewayConfiguration configuration, Invoice invoice)
+        {
+            return
+                "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tem=\"http://tempuri.org/\">" +
+                "<soapenv:Header/>" +
+                "<soapenv:Body>" +
+                "<tem:PinPaymentRequest>" +
+                "<!--Optional:-->" +
+                $"<tem:pin>{configuration.Pin}</tem:pin>" +
+                $"<tem:amount>{invoice.Amount}</tem:amount>" +
+                $"<tem:orderId>{invoice.OrderNumber}</tem:orderId>" +
+                "<!--Optional:-->" +
+                $"<tem:callbackUrl>{invoice.CallbackUrl}</tem:callbackUrl>" +
+                "<tem:authority>0</tem:authority>" +
+                "<tem:status>0</tem:status>" +
+                "</tem:PinPaymentRequest>" +
+                "</soapenv:Body>" +
+                "</soapenv:Envelope>";
+        }
+
+        public static RequestResult CreateRequestResult(string webServiceResponse)
+        {
+            var authority = Convert.ToInt64(XmlHelper.GetNodeValueFromXml(webServiceResponse, "authority", "http://tempuri.org/"));
+
+            var status = Convert.ToByte(XmlHelper.GetNodeValueFromXml(webServiceResponse, "status", "http://tempuri.org/"));
+
+            var isSucceed = status == 0;
+
+            IGatewayResultTranslator gatewayResultTranslator = new ParsianStatusTranslator();
+
+            var translatedResult = gatewayResultTranslator.Translate(status);
+
+            if (!isSucceed)
+            {
+                return new RequestResult(RequestResultStatus.Failed, translatedResult, authority.ToString());
+            }
+
+            var paymentPageUrl = $"{PaymentPageUrl}?au={authority}";
+
+            return new RequestResult(RequestResultStatus.Success, translatedResult, authority.ToString())
+            {
+                BehaviorMode = GatewayRequestBehaviorMode.Redirect,
+                Content = paymentPageUrl
+            };
+        }
+
+        public static ParsianCallbackResult CreateCallbackResult(GatewayVerifyPaymentContext context)
+        {
+            var referenceId = context.RequestParameters.GetAs<string>("au");
+
+            var response = context.RequestParameters.GetAs<string>("rs");
+
+            var isSuccess = !referenceId.IsNullOrWhiteSpace() && !response.IsNullOrWhiteSpace() && response != "0";
+
+            VerifyResult verifyResult = null;
+
+            if (!isSuccess)
+            {
+                IGatewayResultTranslator translator = new ParsianStatusTranslator();
+
+                var translatedMessage = translator.Translate(Convert.ToByte(response));
+
+                verifyResult = new VerifyResult(Gateway.Parsian, referenceId, string.Empty, VerifyResultStatus.Failed, translatedMessage);
+            }
+
+            return new ParsianCallbackResult
+            {
+                IsSucceed = isSuccess,
+                Authority = referenceId,
+                Result = verifyResult
+            };
+        }
+
+        public static string CreateVerifyData(ParsianGatewayConfiguration configuration, ParsianCallbackResult callbackResult)
+        {
+            return
+                "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:tem=\"http://tempuri.org/\">" +
+                "<soap:Header/>" +
+                "<soap:Body>" +
+                "<tem:PaymentEnquiry>" +
+                "<!--Optional:-->" +
+                $"<tem:pin>{configuration.Pin}</tem:pin>" +
+                $"<tem:authority>{callbackResult.Authority}</tem:authority>" +
+                "<tem:status>0</tem:status>" +
+                "<tem:invoiceNumber>0</tem:invoiceNumber>" +
+                "</tem:PaymentEnquiry>" +
+                "</soap:Body>" +
+                "</soap:Envelope>";
+        }
+
+        public static VerifyResult CreateVerifyResult(string webServiceResponse, ParsianCallbackResult callbackResult)
+        {
+            var status = Convert.ToByte(XmlHelper.GetNodeValueFromXml(webServiceResponse, "status", "http://tempuri.org/"));
+
+            //  InvoiceNumber equals to TransactionId in Parbad system.
+            var transactionId = XmlHelper.GetNodeValueFromXml(webServiceResponse, "invoiceNumber", "http://tempuri.org/");
+
+            IGatewayResultTranslator translator = new ParsianStatusTranslator();
+
+            var translatedMessage = translator.Translate(status);
+
+            var isSuccess = status == 0;
+
+            var verifyResultStatus = isSuccess ? VerifyResultStatus.Success : VerifyResultStatus.Failed;
+
+            return new VerifyResult(Gateway.Parsian, callbackResult.Authority, transactionId, verifyResultStatus, translatedMessage);
+        }
+
+        public static string CreateRefundData(ParsianGatewayConfiguration configuration, GatewayRefundPaymentContext context)
+        {
+            return
+                "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tem=\"http://tempuri.org/\">" +
+                "<soapenv:Header/>" +
+                "<soapenv:Body>" +
+                "<tem:PinRefundPayment>" +
+                "<!--Optional:-->" +
+                $"<tem:pin>{configuration.Pin}</tem:pin>" +
+                $"<tem:orderId>{context.OrderNumber}</tem:orderId>" +
+                $"<tem:orderToRefund>{context.OrderNumber}</tem:orderToRefund>" +
+                $"<tem:amount>{context.Amount}</tem:amount>" +
+                "<tem:status>0</tem:status>" +
+                "</tem:PinRefundPayment>" +
+                "</soapenv:Body>" +
+                "</soapenv:Envelope>";
+        }
+
+        public static RefundResult CreateRefundResult(string webServiceResponse, GatewayRefundPaymentContext context)
+        {
+            var status = Convert.ToByte(XmlHelper.GetNodeValueFromXml(webServiceResponse, "status", "http://tempuri.org/"));
+
+            IGatewayResultTranslator translator = new ParsianStatusTranslator();
+
+            var translatedMessage = translator.Translate(status);
+
+            var refundStatus = status == 0 ? RefundResultStatus.Success : RefundResultStatus.Failed;
+
+            return new RefundResult(Gateway.Parsian, context.Amount, refundStatus, translatedMessage);
+        }
+    }
+}
