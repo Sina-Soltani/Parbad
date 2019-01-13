da5ef9e171bfc56f4e7c6f3dd1b270ddc4ca9262
Sina Soltani <soltani.sina1989@live.com>
2019-01-13 16:52:44 +0100

    Reduce Code Duplication. Code clean up (Pasargad).

---
+++ src/Parbad/Providers/Pasargad/Models/PasargadCallbackResult.cs
@@ -0,0 +1,22 @@
+﻿using Parbad.Core;
+
+namespace Parbad.Providers.Pasargad.Models
+{
+    internal class PasargadCallbackResult
+    {
+        public bool IsSucceed { get; set; }
+
+        /// <summary>
+        /// Equals to ReferenceID in Parbad system.
+        /// </summary>
+        public string InvoiceNumber { get; set; }
+
+        public string InvoiceDate { get; set; }
+
+        public string TransactionId { get; set; }
+
+        public string CallbackCheckData { get; set; }
+
+        public VerifyResult Result { get; set; }
+    }
+}
---
+++ src/Parbad/Providers/Pasargad/Models/PasargadCheckCallbackResult.cs
@@ -0,0 +1,11 @@
+﻿using Parbad.Core;
+
+namespace Parbad.Providers.Pasargad.Models
+{
+    internal class PasargadCheckCallbackResult
+    {
+        public bool IsSucceed { get; set; }
+
+        public VerifyResult Result { get; set; }
+    }
+}
