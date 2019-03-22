ad6d09d9fd9fa69f78bfecf0027abb46c1b9d81b
Sina Soltani <soltani.sina1989@live.com>
2019-03-22 19:55:12 +0100

    Clean old codes.

--- src/Parbad/GatewayProviders/ParbadVirtual/MiddlewareInvoker/IParbadVirtualGatewayMiddlewareInvoker.cs
+++ src/Parbad/GatewayProviders/ParbadVirtual/MiddlewareInvoker/IParbadVirtualGatewayMiddlewareInvoker.cs
@@ -2,14 +2,11 @@
 // Licensed under the GNU GENERAL PUBLIC License, Version 3.0. See License.txt in the project root for license information.
 
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 
 namespace Parbad.GatewayProviders.ParbadVirtual.MiddlewareInvoker
 {
     public interface IParbadVirtualGatewayMiddlewareInvoker
     {
-        Task Invoke(HttpContext httpContext);
         Task InvokeAsync();
-        void Invoke2(HttpContext httpContext);
     }
 }
--- src/Parbad/Internal/ParbadVirtualGatewayMiddlewareInvoker.cs
+++ src/Parbad/Internal/ParbadVirtualGatewayMiddlewareInvoker.cs
@@ -21,32 +21,6 @@ namespace Parbad.Internal
             _httpContextAccessor = httpContextAccessor;
         }
 
-        public Task Invoke(HttpContext httpContext)
-        {
-            if (httpContext == null) throw new ArgumentNullException(nameof(httpContext));
-
-            HttpResponseUtilities.AddNecessaryContents(httpContext, "text/html");
-
-            var commandDetails = GetCommandDetails(httpContext);
-
-            if (commandDetails == null)
-            {
-                return httpContext.Response.WriteAsync("Command details are not valid.");
-            }
-
-            switch (commandDetails.CommandType)
-            {
-                case VirtualGatewayCommandType.Request:
-                    return HandleRequestCommand(httpContext, commandDetails);
-
-                case VirtualGatewayCommandType.Pay:
-                    return HandlePayCommand(httpContext, commandDetails);
-
-                default:
-                    return httpContext.Response.WriteAsync("CommandType is not valid.");
-            }
-        }
-
         public Task InvokeAsync()
         {
             var httpContext = _httpContextAccessor.HttpContext;
@@ -70,36 +44,6 @@ namespace Parbad.Internal
 
                 default:
                     return httpContext.Response.WriteAsync("CommandType is not valid.");
-            }
-        }
-
-        public void Invoke2(HttpContext httpContext)
-        {
-            if (httpContext == null) throw new ArgumentNullException(nameof(httpContext));
-
-            HttpResponseUtilities.AddNecessaryContents(httpContext, "text/html");
-
-            var commandDetails = GetCommandDetails(httpContext);
-
-            if (commandDetails == null)
-            {
-                httpContext.Response.WriteAsync("Command details are not valid.").ConfigureAwait(false).GetAwaiter().GetResult();
-                return;
-            }
-
-            switch (commandDetails.CommandType)
-            {
-                case VirtualGatewayCommandType.Request:
-                    HandleRequestCommand(httpContext, commandDetails);
-                    break;
-
-                case VirtualGatewayCommandType.Pay:
-                    HandlePayCommand(httpContext, commandDetails);
-                    break;
-
-                default:
-                    httpContext.Response.WriteAsync("CommandType is not valid.");
-                    break;
             }
         }
 
@@ -155,66 +99,5 @@ namespace Parbad.Internal
 
             return new VirtualGatewayCommandDetails(commandType, trackingNumber, amount, redirectUrl);
         }
-
-        //private Task HandleRequestCommand(HttpContext httpContext, VirtualGatewayCommandDetails commandDetails)
-        //{
-        //    var html = Properties.Resources.VirtualGatewayRequestHtml
-        //        .Replace("#VirtualGatewayPath#", _options.Value.GatewayPath)
-        //        .Replace("#TrackingNumber#", commandDetails.TrackingNumber.ToString())
-        //        .Replace("#Amount#", commandDetails.Amount.ToString())
-        //        .Replace("#DisplayAmount#", ((long)commandDetails.Amount).ToString("N0"))
-        //        .Replace("#RedirectUrl#", commandDetails.RedirectUrl)
-        //        .Replace("#YearNow#", DateTime.Now.Year.ToString());
-
-        //    return httpContext.Response.WriteAsync(html);
-        //}
-
-        //private Task HandlePayCommand(HttpContext httpContext, VirtualGatewayCommandDetails commandDetails)
-        //{
-        //    if (!httpContext.Request.Form.TryGetValue("isPaid", out var isPaid) ||
-        //        !bool.TryParse(isPaid, out var boolIsPaid))
-        //    {
-        //        return httpContext.Response.WriteAsync($"{nameof(isPaid)} field is not valid.");
-        //    }
-
-        //    var html = Properties.Resources.VirtualGatewayResultHtml
-        //        .Replace("#TrackingNumber#", commandDetails.TrackingNumber.ToString())
-        //        .Replace("#DisplayAmount#", ((long)commandDetails.Amount).ToString("N0"))
-        //        .Replace("#TransactionCode#", boolIsPaid ? Guid.NewGuid().ToString("N") : string.Empty)
-        //        .Replace("#RedirectUrl#", commandDetails.RedirectUrl)
-        //        .Replace("#IsPaid#", isPaid.ToString().ToLower())
-        //        .Replace("#ThisYear#", DateTime.Now.Year.ToString())
-        //        .Replace("#CssStatusName#", boolIsPaid ? "success" : "danger")
-        //        .Replace("#StatusText#", boolIsPaid ? "Succeed" : "Failed");
-
-        //    return httpContext.Response.WriteAsync(html);
-        //}
-
-        //private static VirtualGatewayCommandDetails GetCommandDetails(HttpContext httpContext)
-        //{
-        //    if (!Enum.TryParse(httpContext.Request.Form["commandType"], out VirtualGatewayCommandType commandType))
-        //    {
-        //        commandType = VirtualGatewayCommandType.Request;
-        //    }
-
-        //    if (!long.TryParse(httpContext.Request.Form["trackingNumber"], out var trackingNumber))
-        //    {
-        //        return null;
-        //    }
-
-        //    var redirectUrl = httpContext.Request.Form["redirectUrl"];
-
-        //    if (!Money.TryParse(httpContext.Request.Form["amount"], out var amount))
-        //    {
-        //        return null;
-        //    }
-
-        //    if (string.IsNullOrEmpty(redirectUrl))
-        //    {
-        //        return null;
-        //    }
-
-        //    return new VirtualGatewayCommandDetails(commandType, trackingNumber, amount, redirectUrl);
-        //}
     }
 }
