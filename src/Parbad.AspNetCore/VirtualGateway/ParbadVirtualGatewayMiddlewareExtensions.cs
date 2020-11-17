6d3f9176eaf1ccb63e8772dff5dee47da9460421
Sina Soltani <soltani.sina1989@live.com>
2020-11-17 13:23:09 +0100

    Code cleanup.

--- src/Parbad.AspNetCore/VirtualGateway/ParbadVirtualGatewayMiddlewareExtensions.cs
+++ src/Parbad.AspNetCore/VirtualGateway/ParbadVirtualGatewayMiddlewareExtensions.cs
@@ -21,7 +21,7 @@ namespace Microsoft.AspNetCore.Builder
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
 
-#if NETCOREAPP3_0
+#if NETCOREAPP3_0 || Net_5
             var hostEnvironment = builder.ApplicationServices.GetRequiredService<Hosting.IWebHostEnvironment>();
             var isDevelopment = Microsoft.Extensions.Hosting.HostEnvironmentEnvExtensions.IsDevelopment(hostEnvironment);
 #else
