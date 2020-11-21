5f73a6c83cd40f65d19e9dcb2332f9b80148d5a6
Sina Soltani <soltani.sina1989@live.com>
2020-11-21 16:54:39 +0100

    UnitTest.

--- src/Parbad/tests/Gateway/Saman/SamanGatewayTests.cs
+++ src/Parbad/tests/Gateway/Saman/SamanGatewayTests.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Parbad.Tests.Gateway.Saman
 {
-    
+    [TestClass]
     public class SamanGatewayTests
     {
     }
