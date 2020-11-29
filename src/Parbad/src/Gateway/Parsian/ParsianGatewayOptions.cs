2f29dcb929f5446fc100582a75961808b6b53cc6
Sina Soltani <soltani.sina1989@live.com>
2020-11-29 12:23:58 +0100

    Parsian Gateway Options.

--- src/Parbad/src/Gateway/Parsian/ParsianGatewayOptions.cs
+++ src/Parbad/src/Gateway/Parsian/ParsianGatewayOptions.cs
@@ -2,12 +2,12 @@
 {
     public class ParsianGatewayOptions
     {
-        public string PaymentPageUrl = "https://pec.shaparak.ir/NewIPG/";
+        public string PaymentPageUrl { get; set; } = "https://pec.shaparak.ir/NewIPG/";
 
-        public string ApiRequestUrl = "https://pec.shaparak.ir/NewIPGServices/Sale/SaleService.asmx";
+        public string ApiRequestUrl { get; set; } = "https://pec.shaparak.ir/NewIPGServices/Sale/SaleService.asmx";
 
-        public string ApiVerificationUrl = "https://pec.shaparak.ir/NewIPGServices/Confirm/ConfirmService.asmx";
+        public string ApiVerificationUrl { get; set; } = "https://pec.shaparak.ir/NewIPGServices/Confirm/ConfirmService.asmx";
 
-        public string ApiRefundUrl = "https://pec.shaparak.ir/NewIPGServices/Reverse/ReversalService.asmx";
+        public string ApiRefundUrl { get; set; } = "https://pec.shaparak.ir/NewIPGServices/Reverse/ReversalService.asmx";
     }
 }
