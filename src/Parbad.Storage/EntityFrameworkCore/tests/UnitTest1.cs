65a86085b69b25a1aa87cc8b1a261124f121a8fd
Sina Soltani <soltani.sina1989@live.com>
2020-12-12 19:21:19 +0100

    Adding UnitTests.

---
+++ src/Parbad.Storage/EntityFrameworkCore/tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Parbad.Storage.EntityFrameworkCore.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
