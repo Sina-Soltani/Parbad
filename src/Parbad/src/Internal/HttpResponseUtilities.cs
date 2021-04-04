3dd339b93ba14b190cd8788da0a89593b74daa78
Sina Soltani <soltani.sina1989@live.com>
2021-04-04 13:12:02 +0200

    Removing WebUtilities nuget package.

--- src/Parbad/src/Internal/HttpResponseUtilities.cs
+++ src/Parbad/src/Internal/HttpResponseUtilities.cs
@@ -2,7 +2,6 @@
 // Licensed under the GNU GENERAL PUBLIC License, Version 3.0. See License.txt in the project root for license information.
 
 using Microsoft.AspNetCore.Http;
-using Microsoft.Net.Http.Headers;
 
 namespace Parbad.Internal
 {
@@ -12,9 +11,9 @@ namespace Parbad.Internal
         {
             var response = httpContext.Response;
 
-            response.Headers[HeaderNames.CacheControl] = "no-store, no-cache";
-            response.Headers[HeaderNames.Pragma] = "no-cache";
-            response.Headers[HeaderNames.Expires] = "Thu, 01 Jan 1970 00:00:00 GMT";
+            response.Headers["Cache-Control"] = "no-store, no-cache";
+            response.Headers["Pragma"] = "no-cache";
+            response.Headers["Expires"] = "Thu, 01 Jan 1970 00:00:00 GMT";
 
             if (!contentType.IsNullOrEmpty())
             {
