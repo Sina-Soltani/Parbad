775928dcde626ee8c34bed511f14df19363a47c7
Sina Soltani <soltani.sina1989@live.com>
2022-02-19 15:31:30 +0100

    Code cleanup.

--- src/Parbad.Gateway/Zibal/src/ZibalGateway.cs
+++ src/Parbad.Gateway/Zibal/src/ZibalGateway.cs
@@ -1,10 +1,6 @@
 ﻿// Copyright (c) Parbad. All rights reserved.
 // Licensed under the GNU GENERAL PUBLIC License, Version 3.0. See License.txt in the project root for license information.
 
-using System;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
@@ -15,6 +11,10 @@ using Parbad.GatewayBuilders;
 using Parbad.Internal;
 using Parbad.Net;
 using Parbad.Options;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Parbad.Gateway.Zibal
 {
--- src/Parbad.Gateway/Zibal/src/ZibalGatewayInvoiceBuilderExtensions.cs
+++ src/Parbad.Gateway/Zibal/src/ZibalGatewayInvoiceBuilderExtensions.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) Parbad. All rights reserved.
 // Licensed under the GNU GENERAL PUBLIC License, Version 3.0. See License.txt in the project root for license information.
 
-using System;
 using Parbad.Abstraction;
 using Parbad.Gateway.Zibal.Internal;
 using Parbad.InvoiceBuilder;
+using System;
 
 namespace Parbad.Gateway.Zibal
 {
